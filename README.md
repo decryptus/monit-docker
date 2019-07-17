9a4f8340f0886c6a06ad10402cf815fb145a4ce6
Adrien Delle Cave <adrien.delle.cave@commandersact.com>
2019-07-17 16:54:46 +0200

    [RELEASE] version: 0.0.5

---
+++ README.md
@@ -0,0 +1,31 @@
+# monit-docker project
+
+## Installation
+
+pip install monit-docker
+
+## Commands:
+
+Restart containers with name starts with foo if memory percent > 60% or cpu percent > 90%:
+
+`monit-docker monit --name "foo*" --cmd-if 'mem_percent > 60 ? restart' --cmd-if 'cpu_percent > 90 ? restart'`
+
+Restart containers with name starts with bar and foo if cpu percent greather than 60% and less than 70%:
+
+`monit-docker monit --name "bar*" --name "foo*" --cmd-if '60 > cpu_percent < 70 ? restart'`
+
+Restart containers with name starts with bar and status equal to pause or running:
+
+`monit-docker monit --name "bar*" --cmd-if 'status in (pause,running) ? restart'`
+
+Run command in container with image name contains /php-fpm/ and if memory percent > 90%:
+
+`monit-docker --image '*/php-fpm/*' monit --cmd-if 'mem_percent > 90 ? (kill -USR2 1)'`
+
+#### With aliases
+
+Run commands with aliases declared in configuration file:
+
+`monit-docker monit --id 4c01db0b339c --cmd-if '@status_not_running ? restart'`
+
+`monit-docker monit --name "foo*" --cmd-if '@status_not_running ? @start_pause'`
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import os
+from setuptools import find_packages, setup
+
+version                = '0.0.5'
+
+current_dir            = os.path.abspath(os.path.dirname(__file__))
+requirements           = [line.strip() for line in open(os.path.join(current_dir, 'requirements.txt'), 'r').readlines()]
+version_file           = os.path.join(current_dir, 'VERSION')
+readme_file            = os.path.join(current_dir, 'README.md')
+long_desc              = None
+long_desc_content_type = None
+
+if os.path.isfile(version_file):
+    version = open(version_file, 'r').readline().strip() or version
+
+if os.path.isfile(readme_file):
+    long_desc = open(readme_file, 'r').read()
+    long_desc_content_type = 'text/markdown'
+
+setup(
+    name                          = 'monit-docker',
+    version                       = version,
+    description                   = 'monit-docker',
+    author                        = 'Adrien Delle Cave',
+    author_email                  = 'pypi@doowan.net',
+    license                       = 'License GPL-2',
+    url                           = 'https://github.com/decryptus/monit-docker',
+    scripts                       = ['bin/monit-docker'],
+    packages                      = find_packages(),
+    install_requires              = requirements,
+    long_description              = long_desc,
+    long_description_content_type = long_desc_content_type
+)
